0d2048b08fbb5a601affc1f07fc7881825da947b
Dylan K. Taylor <dktapps@pmmp.io>
2025-09-15 17:10:52 +0100

    Disable loop interrupt checks for profiling

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -4094,11 +4094,7 @@ ZEND_API ZEND_COLD void ZEND_FASTCALL zend_fcall_interrupt(zend_execute_data *ca
 		} \
 	} while (0)
 
-#define ZEND_VM_LOOP_INTERRUPT_CHECK() do { \
-		if (UNEXPECTED(zend_atomic_bool_load_ex(&EG(vm_interrupt)))) { \
-			ZEND_VM_LOOP_INTERRUPT(); \
-		} \
-	} while (0)
+#define ZEND_VM_LOOP_INTERRUPT_CHECK()
 
 #define ZEND_VM_FCALL_INTERRUPT_CHECK(call) do { \
 		if (UNEXPECTED(zend_atomic_bool_load_ex(&EG(vm_interrupt)))) { \
